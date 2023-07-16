27fd8845e222eeda052bd3f6bbcfbf2aa4801961
Luiza Vidal <lcopolillo@gmail.com>
2023-07-16 01:07:43 +0100

    updates

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,9 @@ from django.urls import path
 app_name = "portfolio"
 
 urlpatterns = [
-    path('home', views.home_page_view, name='home')
+    path('', views.home_page_view, name='home'),
+    path('home', views.home_page_view, name='home'),
+    path('more', views.more_page_view, name='more'),
+    path('grad', views.grad_page_view, name='grad'),
+    path('blog', views.blog_page_view, name='blog'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home_page_view(request):
 	return render(request, 'portfolio/index.html')
+
+def more_page_view(request):
+	return render(request, 'portfolio/more.html')
+
+def grad_page_view(request):
+	return render(request, 'portfolio/licen.html')
+
+def blog_page_view(request):
+	return render(request, 'portfolio/blog.html')
